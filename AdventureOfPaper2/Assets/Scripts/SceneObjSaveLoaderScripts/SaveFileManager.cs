a191892958533a6e4faf76ca8303b8d520d5812c
TaaviM88Laptop <juomakuski@hotmail.com>
2020-09-16 13:46:30 +0300

    scriptejä

---
+++ AdventureOfPaper2/Assets/Scripts/SceneObjSaveLoaderScripts/SaveFileManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+using Extensions;
+
+[Serializable]
+[CreateAssetMenu(menuName ="Custom Assets/Save File manager")]
+public class SaveFileManager : ScriptableObjectSingleton<SaveFileManager>
+{
+    [SerializeField]
+    public SaveFileManager[] SaveFiles;
+
+    [SerializeField]
+    public int CurrentSaveFile;
+}
---
+++ AdventureOfPaper2/Assets/Scripts/SceneObjSaveLoaderScripts/ScriptableObjectSingleton.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Extensions
+{
+    public class ScriptableObjectSingleton<T> : ScriptableObject where T : ScriptableObject
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (!instance)
+                {
+                    instance = Resources.FindObjectsOfTypeAll<T>().FirstOrDefault();
+                }
+                return instance;
+            }
+        }
+    }
+}
+
