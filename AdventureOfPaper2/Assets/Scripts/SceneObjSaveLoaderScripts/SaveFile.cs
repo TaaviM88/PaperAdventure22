cd813052d71ad63c62180c716355adbc4b2e534f
Mutti88 <juomakuski@hotmail.com>
2020-09-16 22:43:41 +0300

    Seuraava askel: LoadGameObjects - script

---
+++ AdventureOfPaper2/Assets/Scripts/SceneObjSaveLoaderScripts/SaveFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(menuName = "Custom Assets/Save File")]
+public class SaveFile : ScriptableObject
+{
+    [SerializeField]
+    public LevelDictionary GameObjectsSateste;
+
+    [SerializeField]
+    public string CharacterName = "Link";
+}
