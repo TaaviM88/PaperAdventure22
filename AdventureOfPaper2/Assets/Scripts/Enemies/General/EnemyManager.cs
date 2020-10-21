0d29985232cb83a50be34418bab3c9d7445824fe
TaaviM88Laptop <juomakuski@hotmail.com>
2020-10-21 13:51:00 +0300

    wax kuolinanimaatio laitettu toimintaan. Pelaajan hyppyä ja juoksu nopeutta muutettu. Pelaajan stab collideria muutettu

---
+++ AdventureOfPaper2/Assets/Scripts/Enemies/General/EnemyManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(EnemyAnimeManager))]
+public class EnemyManager : MonoBehaviour, ITakeDamage<int>, IDie
+{
+    public int health = 2;
+    public int side = -1;
+    public int exp = 2;
+    public ExpToNumberTextMesh expNumber;
+    private EnemyAnimeManager anime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anime = GetComponent<EnemyAnimeManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Damage(int damage)
+    {
+        Debug.Log("Damage was  " + damage);
+        health = Mathf.Min(health - damage, health);
+        anime.SetTrigger("Damaged");
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+
+
+    public void Die()
+    {
+        Debug.Log("kuolin t." + gameObject.name);
+        //tee kuolin animaation ja anna expat
+        anime.SetTrigger("Death");
+        SpawnExpNumber();
+
+    }
+
+    public void DestroyEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    public void SpawnExpNumber()
+    {
+        //Vector3 pos;
+        //float x = Camera.main.ViewportToScreenPoint(transform.position).x;
+        //float y = Camera.main.ViewportToScreenPoint(transform.position).y;
+        //pos = new Vector3(x, y, 0f); 
+        Instantiate(expNumber, transform.position, Quaternion.identity).SetExp(exp);
+    }
+}
