752663d935473b1233c16cdcba3cf1cb28ba1ea9
TaaviM88Laptop <juomakuski@hotmail.com>
2020-11-11 13:55:12 +0200

    BossroomManager started

---
+++ AdventureOfPaper2/Assets/Scripts/Managers/BossRoomManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+public class BossRoomManager : MonoBehaviour
+{
+    public GameObject doorRight;
+    public GameObject doorLeft;
+    public List<Light2D> lights2D = new List<Light2D>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
